ff831a0a120eb69dd22fd025652ab00c5788807f
Richard Isaac <oluwalekerichardisaac@gmail.com>
2023-03-07 11:33:49 +0100

    ....

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -60,9 +60,7 @@ const Hero = ({
       <div className="container-sm">
         <div className={innerClasses}>
           <div className="hero-content">
-            <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-      
-Accelerating African Startups, Creators & Businesses   <span className="text-color-primary">for peak profit  </span>
+            <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">Accelerating African Startups, Creators & Businesses   <span className="text-color-primary">for peak profit  </span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
