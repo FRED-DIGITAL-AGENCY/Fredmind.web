0a0eda462fc5a939df01bd5e3289ff6b3b562efb
Richard Isaac <69818214+Guruscode@users.noreply.github.com>
2023-03-30 08:13:23 -0400

    Added Build Folder

---
+++ build/precache-manifest.233ddb673c043a507190e4cec141c655.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e35c82f35105acc014d4090b1649e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9415364f53fcba659ec",
+    "url": "/static/css/main.a6a7d02f.chunk.css"
+  },
+  {
+    "revision": "8f182d6acda31c1f966f",
+    "url": "/static/js/2.d492a322.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.d492a322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9415364f53fcba659ec",
+    "url": "/static/js/main.e0d005d3.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "17cc5dc007c637dc0a8531e95865bcd4",
+    "url": "/static/media/features-split-image-01.17cc5dc0.png"
+  },
+  {
+    "revision": "554c635713fd8bb5f60b75279879a4e3",
+    "url": "/static/media/features-split-image-02.554c6357.png"
+  },
+  {
+    "revision": "b91a35cd56c5a1e9dd140f884bceded8",
+    "url": "/static/media/features-split-image-03.b91a35cd.png"
+  },
+  {
+    "revision": "531a780029f641d1b6f7b145303df3cc",
+    "url": "/static/media/hero-pattern-bg.531a7800.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "fdf015f917829be3a21f6b03e3e9b829",
+    "url": "/static/media/video-placeholder.fdf015f9.jpg"
+  }
+]);
