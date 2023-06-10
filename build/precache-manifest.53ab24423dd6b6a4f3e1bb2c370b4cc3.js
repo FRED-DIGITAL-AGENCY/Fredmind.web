98f5996e15a5f7ad4792e7bf99de8f00f70f9905
Richard Isaac <oluwalekerichardisaac@gmail.com>
2023-06-10 12:35:01 +0100

    Fix: Added Build To Help Resolve Failed Deployment

---
+++ build/precache-manifest.53ab24423dd6b6a4f3e1bb2c370b4cc3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac79390d70cc1b79b06eaf30bc35372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72a5d4526fd3ad7dadca",
+    "url": "/static/css/2.cff1cea2.chunk.css"
+  },
+  {
+    "revision": "14a48393c1eaa367abe8",
+    "url": "/static/css/main.a6a7d02f.chunk.css"
+  },
+  {
+    "revision": "72a5d4526fd3ad7dadca",
+    "url": "/static/js/2.e75f17d6.chunk.js"
+  },
+  {
+    "revision": "fb78fb2cee6e8f5ff1266c307ae7be87",
+    "url": "/static/js/2.e75f17d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a48393c1eaa367abe8",
+    "url": "/static/js/main.3d66100e.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "ddedfb335fc0449ad61aba5d4870de7a",
+    "url": "/static/media/Fred.ddedfb33.jpg"
+  },
+  {
+    "revision": "c88c8faf384f5729fee24e70df47f680",
+    "url": "/static/media/K.c88c8faf.jpg"
+  },
+  {
+    "revision": "693788c104e43d265f2810f40b240c06",
+    "url": "/static/media/Tomi.693788c1.jpg"
+  },
+  {
+    "revision": "0b2c33884f733675dd111e82aa916292",
+    "url": "/static/media/Yomi.0b2c3388.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "e4a59e23706451dfa8367312e48792d0",
+    "url": "/static/media/emmy.e4a59e23.jpg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "17cc5dc007c637dc0a8531e95865bcd4",
+    "url": "/static/media/features-split-image-01.17cc5dc0.png"
+  },
+  {
+    "revision": "554c635713fd8bb5f60b75279879a4e3",
+    "url": "/static/media/features-split-image-02.554c6357.png"
+  },
+  {
+    "revision": "b91a35cd56c5a1e9dd140f884bceded8",
+    "url": "/static/media/features-split-image-03.b91a35cd.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "426a2b70f928ec4dcec7e63e34e383dd",
+    "url": "/static/media/richard.426a2b70.jpg"
+  },
+  {
+    "revision": "fdf015f917829be3a21f6b03e3e9b829",
+    "url": "/static/media/video-placeholder.fdf015f9.jpg"
+  },
+  {
+    "revision": "ed44dc5d1d3df4597fbdd91513f53382",
+    "url": "/static/media/video1.ed44dc5d.mp4"
+  },
+  {
+    "revision": "19a790ff56357b4a7f85566ff723322e",
+    "url": "/static/media/vision.19a790ff.jpg"
+  }
+]);
